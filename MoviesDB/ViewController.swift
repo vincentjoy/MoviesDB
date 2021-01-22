309ed34dcbdeb934eeff9be7b2e186579c34f809
Rehlat Online Services Pvt Ltd <joy.vincent@rehlat.com>
2021-01-22 19:43:56 +0530

    Initial Commit

---
+++ MoviesDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesDB
+//
+//  Created by Rehlat Online Services Pvt Ltd on 22/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
